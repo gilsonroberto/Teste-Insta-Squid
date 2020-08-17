764b585799b901f37846619c99b1559bd63dcbd5
gilsonroberto <gilsonroberto@gmail.com>
2020-08-17 19:59:54 -0300

    Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Código que será executado quando o navegador carregar
+  const API_URL = "https://vision.squidit.com.br/v1/feed/test?count=50";
+  const container = document.querySelector(".container");
+  const load = document.querySelector("#load");
+
+
+});
