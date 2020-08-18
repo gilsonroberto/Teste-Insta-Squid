f98e768e3e849006d23171f7f8f47ddcc025109e
gilsonroberto <gilsonroberto@gmail.com>
2020-08-18 11:22:18 -0300

    Commit layout

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Código que será executado quando o navegador carregar
-  const API_URL = "https://vision.squidit.com.br/v1/feed/test?count=50";
+  const API_URL = "https://vision.squidit.com.br/v1/feed/test?count=100";
   const load = document.querySelector("#load");
   const container = document.querySelector(".container");
   
